59ba113a33109d073a013d3d9258840e6d7a07fe
Qian Liyang <liyang.19@intl.zju.edu.cn>
2023-03-03 01:41:14 +0800

    webDriveWait required

--- wundergroundCrawler/wunderGroundCrawler.py
+++ wundergroundCrawler/wunderGroundCrawler.py
@@ -9,6 +9,7 @@
 1. 使用 date_range方法创建查询时间序列query_list
 2. dropna(axis=0, how='all')方法可以删除值全为空的行
 '''
+import time
 
 # In[2]:
 
@@ -22,14 +23,18 @@ from tqdm import tqdm
 
 
 # In[3]:
-
+PORT = 7890
 
 # 输入查询的起止日期
-start_date = input('请输入查询天气起始日期（8位数字）：')
-end_date = input('请输入查询天气终止日期（8位数字）：')
+# start_date = input('请输入查询天气起始日期（8位数字）：')
+# end_date = input('请输入查询天气终止日期（8位数字）：')
+start_date = "20230302"
+end_date = "20230302"
 # 处理起止日期
 struct_start_date = datetime.datetime.strptime(start_date, '%Y%m%d')
 struct_end_date = datetime.datetime.strptime(end_date, '%Y%m%d')
+
+proxies = {'http': 'http://127.0.0.1:{}'.format(PORT), 'https': 'http://127.0.0.1:{}'.format(PORT)}
 
 
 # In[4]:
@@ -42,30 +47,36 @@ def get_weather(url):
     }
     # 两种方法可以获得html
     # 方法1 requests库
-    resp = requests.get(url, headers=headers)
+    print("start crawling")
+    time.sleep(5)
+    resp = requests.get(url, headers=headers, proxies=proxies)
     if resp.status_code == 200:
         html = resp.text
-        '''
-        # 方法2 requests_html库
-        session = HTMLSession()
-        resp = session.get(url, headers=headers)
-        html = resp.html.html
-        '''
         df_list = pd.read_html(html)
-        weather = df_list[3]  # 一个一个试出来，第四个表格为我需要的天气表格
-        weather.dropna(axis=0, how='all')  # 删除值全为空的行
+        weather = df_list[0]  # 一个一个试出来，第四个表格为我需要的天气表格
+        weather = weather.dropna(axis=0, how='all')  # 删除值全为空的行
         return True, weather
     else:
         return False  # 如果异常，获取失败，返回None
+
+    # 方法2 requests_html库
+    # session = HTMLSession()
+    # resp = session.get(url, headers=headers)
+    # html = resp.html.html
 
 
 def generate_url(date):
     # 创建查询url
     # 分析查询网页的url，每次需要改变两个位置，个人天气站点是ISHANG46
-    # url = 'https://www.wunderground.com/dashboard/pws/ISHANG46/table/2021-10-9/2021-10-9/daily'
+    # https://www.wunderground.com/history/daily/ZSHC/date/2023-3-2
     str_date = date.strftime('%Y-%m-%d')
-    weather_station = 'ISHANG46'
-    url = 'https://www.wunderground.com/dashboard/pws/' + weather_station + '/table/' + str_date + '/' + str_date + '/daily'
+    # date_obj = datetime.datetime.strptime(str_date, "%Y-%m-%d")
+    # print(date_obj)
+    # formatted_date = date_obj.strftime("%Y-%-m-%-d")
+    weather_station = 'ZSHC'
+    url = "https://www.wunderground.com/history/daily/{0}/{1}/{2}".format(weather_station, 'date', str_date)
+    print()
+    print(url)
     return url
 
 
@@ -85,7 +96,6 @@ def get_all_weather():
     '''
 
     query_list = [d for d in pd.date_range(struct_start_date, struct_end_date, freq='D')]
-    print(query_list)
     df_list = []
     for j in tqdm(range(len(query_list))):
         i = query_list[j]
