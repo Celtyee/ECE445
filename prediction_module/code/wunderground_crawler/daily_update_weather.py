b877b96c79050e0260b42111911084ae770625da
liyangq2 <liyangq2@illinois.edu>
2023-04-13 18:47:57 +0800

    Update daily_update_weather.py

--- prediction_module/code/wunderground_crawler/daily_update_weather.py
+++ prediction_module/code/wunderground_crawler/daily_update_weather.py
@@ -26,6 +26,8 @@ def daily_update_weather(driver_path, save_folder, context_len=30):
 def main():
     google_driver_path = "./chromedriver_win32/chromedriver.exe"
     weather_folder = "../../data/weather/history"
+    if not os.path.exists(weather_folder):
+        os.mkdir(weather_folder)
     daily_update_weather(google_driver_path, weather_folder)
 
 
